33f86c8a916f7490fa13971863fdca4ed2e0434c
DESKTOP-B69HBFL\dante <macholak.piotr@gmail.com>
2018-05-04 17:49:09 +0200

    a

--- Java/Fifi/src/Open_Save.java
+++ Java/Fifi/src/Open_Save.java
@@ -7,15 +7,28 @@ public class Open_Save
 {
 	static String string_zmienna;
 	private static Scanner in;
+	static int zmienna;
+	static int i;
+	static int tab[];
 	
 	
 	public static void main(String[] args) throws FileNotFoundException 
 	{
+		tab = new int [15];
+		
 		File file = new File("D:\\My Apps\\Teaching Apps\\Java\\Fifi\\bin\\zad_15.txt");
 		in = new Scanner(file);
 		
+		for(i=0; i<15; i++)
+		{
 		string_zmienna = in.nextLine();
-		System.out.println("Pierwsza linijka to " + string_zmienna);
+		zmienna = Integer.parseInt(string_zmienna);
+		tab[i] = zmienna;
+		}
+		for(i=0; i<15; i++)
+		{
+			System.out.println("Element tablicy " + i + " to " + tab[i]);
+		}
 	}
 
 }
