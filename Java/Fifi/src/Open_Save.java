891559a857b87f8610354807b7a46fcd226edb66
DESKTOP-B69HBFL\dante <macholak.piotr@gmail.com>
2018-05-04 17:08:20 +0200

    Added

--- Java/Fifi/src/Open_Save.java
+++ Java/Fifi/src/Open_Save.java
@@ -1,9 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class Open_Save {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Open_Save 
+{
+	static String string_zmienna;
+	private static Scanner in;
+	
+	
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		File file = new File("D:\\My Apps\\Teaching Apps\\Java\\Fifi\\bin\\zad_15.txt");
+		in = new Scanner(file);
+		
+		string_zmienna = in.nextLine();
+		System.out.println("Pierwsza linijka to " + string_zmienna);
 	}
 
 }
